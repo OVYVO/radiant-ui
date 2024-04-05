cbfb7b3fba32924656c08d62680aa86206a17b2b
ryanYan <vue12306@163.com>
2024-04-05 14:25:36 +0800

    :sparkles: feat: 添加eslint及prettier工具

--- packages/components/icon/src/icon.vue
+++ packages/components/icon/src/icon.vue
@@ -5,23 +5,22 @@
 </template>
 
 <script setup lang="ts">
-import { createNameSpace } from "@radiant/utils/bem";
-import { iconProps } from "./icon";
-import { computed } from "vue";
-
+import { createNameSpace } from "@radiant/utils/bem"
+import { iconProps } from "./icon"
+import { computed } from "vue"
 defineOptions({
-  name: "ra-icon",
-});
+  name: "ra-icon"
+})
 
-const bem = createNameSpace("icon");
+const bem = createNameSpace("icon")
 
-const props = defineProps(iconProps);
+const props = defineProps(iconProps)
 
 const computedStyle = computed(() => {
-  if (!props.color && !props.size) return {};
+  if (!props.color && !props.size) return {}
   return {
     color: props.color ? props.color : "",
-    fontSize: props.size ? `${props.size}px` : "",
-  };
-});
+    fontSize: props.size ? `${props.size}px` : ""
+  }
+})
 </script>
