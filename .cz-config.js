00fd22fd2d613b02b0e6276f2e3eb67339ac6671
ryanYan <vue12306@163.com>
2024-04-05 19:58:48 +0800

    :sparkles: feat: 新增commitlint husky功能

---
+++ .cz-config.js
@@ -0,0 +1,20 @@
+//https://github.com/leoforfree/cz-customizable/blob/master/cz-config-EXAMPLE.js
+process.env.GITMOJI_PATH = ".gitmoji.json"
+
+module.exports = {
+  types: [
+    { value: ":sparkles: feat", name: "✨ feat: 一项新功能" },
+    { value: ":bug: fix", name: "🐛 fix: 修复一个Bug" },
+    { value: ":memo: docs", name: "📝 docs: 文档变更" },
+    { value: ":lipstick: style", name: "💄 style: 代码风格，格式修复" },
+    { value: ":zap: perf", name: "⚡️ perf: 代码优化,改善性能" },
+    { value: ":rocket: chore", name: "🚀 chore: 变更构建流程或辅助工具" },
+    { value: ":package: build", name: "📦️ build: 变更项目构建或外部依赖" }
+  ],
+  messages: {
+    type: "请选择提交类型(必填):",
+    subject: "请简要描述提交(必填):",
+    confirmCommit: "确定提交此说明吗？"
+  },
+  skipQuestions: ["scope", "body", "breaking", "footer"]
+}
