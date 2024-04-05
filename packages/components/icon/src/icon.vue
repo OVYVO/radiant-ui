8c85541fa0833a97d0f6a98250b0e6a5642d9a88
ryanYan <vue12306@163.com>
2024-04-05 19:24:20 +0800

    :zap: perf: perf

--- packages/components/icon/src/icon.vue
+++ packages/components/icon/src/icon.vue
@@ -8,12 +8,12 @@
 import { createNameSpace } from "@radiant/utils/bem"
 import { iconProps } from "./icon"
 import { computed } from "vue"
+
 defineOptions({
   name: "ra-icon"
 })
 
 const bem = createNameSpace("icon")
-
 const props = defineProps(iconProps)
 
 const computedStyle = computed(() => {
