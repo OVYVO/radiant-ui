273536a5aefea1188053a9dd51af9d5b9dda4829
ryanYan <vue12306@163.com>
2024-04-16 15:00:37 +0800

    :lipstick: style: stylelint添加bem校验

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -1,7 +1,19 @@
 module.exports = {
   extends: ["stylelint-config-standard-scss", "@ovyvo/stylelint-config"],
   rules: {
-    "scss/dollar-variable-pattern": null
+    "scss/dollar-variable-pattern": null,
+    "plugin/selector-bem-pattern": {
+      componentName: "[A-Z]+",
+      preset: "bem",
+      componentSelectors: {
+        //初始选择器规则，可理解为顶层class规则
+        initial: "^\\.{componentName}(?:__[-a-z]+)?(?:--[a-z]+)?$",
+        //可理解为内层选择器规则
+        combined: "^\\.{componentName}(?:__[-a-z]+)(?:--[a-z]+)?$"
+      },
+      utilitySelectors: "^\\.util-[a-z]+$",
+      ignoreSelectors: ["^\\.el-", "/deep/", ">>>", "^\\.icon-"]
+    }
   },
-  plugins: ["stylelint-scss"]
+  plugins: ["stylelint-scss", "stylelint-selector-bem-pattern"]
 }
