723bff65e24e8f3d99d456ce4e65aec43d71ef3b
ryanYan <vue12306@163.com>
2024-07-30 11:33:40 +0800

    :bug: fix: eslint typescript校验错误问题修复

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,9 @@
 import { defineConfig } from "@ovyvo/eslint-config"
 
-export default defineConfig({ scriptType: "typescript" })
+export default [
+  ...defineConfig({ scriptType: "typescript" }),
+  {
+    files: ["**/*.vue"],
+    parser: "vue-eslint-parser"
+  }
+]
