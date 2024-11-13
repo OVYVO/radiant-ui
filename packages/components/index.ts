c009fb059176ae310932adeaaadb3446c1ea1877
OVYVO <vue12306@163.com>
2024-11-13 10:09:42 +0800

    :bug: fix: 解决全局引入注册方式bug

--- packages/components/index.ts
+++ packages/components/index.ts
@@ -1,1 +1,12 @@
-export * from "./button"
+import type { App } from "vue"
+import RaActbtn from "./button"
+
+const components = [RaActbtn]
+
+export default {
+  install: (app: App) => {
+    components.forEach(item => {
+      if (typeof item.install === "function") item.install(app)
+    })
+  }
+}
