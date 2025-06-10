203fa0c83aa2b1211aefe09df1c152196acf5e7b
OVYVO <vue12306@163.com>
2025-06-10 15:11:30 +0800

    :bug: fix: 优化pkg打包配置

--- packages/cli/utils/pkg.ts
+++ packages/cli/utils/pkg.ts
@@ -1,7 +1,7 @@
 import { projRoot } from "./path"
 
 export const excludeFiles = (files: string[]) => {
-  const excludes = ["node_modules", "test", "mock", "dist", "global.d.ts"]
+  const excludes = ["node_modules", "test", "mock", "dist", "global.d.ts", "images.d.ts"]
   return files.filter(path => {
     const position = path.startsWith(projRoot) ? projRoot.length : 0
     return !excludes.some(exclude => path.includes(exclude, position))
